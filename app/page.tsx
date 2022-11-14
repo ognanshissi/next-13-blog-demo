ab889f3fcfc1572ef822da069cad60338f478d09
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2022-11-14 16:49:07 +0000

    Update article loading

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 className="text-6xl font-bold text-teal-700 border-b-8 pb-4 border-b-teal-700 inline-block mb-4">Recent blog posts</h1>
       {/* Posts */}
       <Suspense fallback={<Spinner/>}>
-        <ArticleList/>
+        <ArticleList />
       </Suspense>
     </div>
   )
